5c3bf031b47a0983ff8e2f4c39c1385db74c2caa
Yam Horin <horinyam@gmail.com>
2025-08-07 22:10:34 +0300

    restart

--- APP/android/app/src/main/kotlin/com/example/pjbooks/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pjbooks
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Server/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "my_secret_key")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/bookgrocer")
